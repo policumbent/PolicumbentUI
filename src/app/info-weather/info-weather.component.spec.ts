ead25755df0506e2fb2600924fd9d0b22319b43d
Stefano Loscalzo <stefano.loscalzo@gmail.com>
2020-12-20 17:43:14 +0100

    weather details charts

---
+++ src/app/info-weather/info-weather.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { InfoWeatherComponent } from './info-weather.component';
+
+describe('InfoWeatherComponent', () => {
+  let component: InfoWeatherComponent;
+  let fixture: ComponentFixture<InfoWeatherComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ InfoWeatherComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(InfoWeatherComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/info-weather/info-weather.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-info-weather',
+  templateUrl: './info-weather.component.html',
+  styleUrls: ['./info-weather.component.css']
+})
+export class InfoWeatherComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
